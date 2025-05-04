0da89299785ef8ba7b0a9abff341c1d01c9c52c4
Mohammad <MBGmohammad@yahoo.com>
2025-05-04 20:25:17 +0330

    change setup 1

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><img src={logo} className="App-logo" alt="logo" /> mmd80519 </h1>
+        <h3> My Site Design </h3>
+        
       </header>
     </div>
   );
